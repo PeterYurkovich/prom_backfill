b330e6cfbb6318afc5983d892094359f6a7874b0
PeterYurkovich <pyurkovi@redhat.com>
2024-03-19 13:04:10 -0400

    Create cake container_network_receive_bytes_total metric

--- prom_backfill.go
+++ prom_backfill.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"math"
+	"math/rand"
 	"os"
 	"time"
 
@@ -67,10 +68,12 @@ func main() {
 	err = os.RemoveAll("tsdb-test")
 	noErr(err)
 
-	fake := randomCNRBT()
+	faker := randomCnrbt()
+
+	fake := faker()
 	fmt.Printf("%+v\n", fake)
 
-	fakeTwo := randomCNRBT()
+	fakeTwo := faker()
 	isEqual := cmp.Equal(fake, fakeTwo)
 	fmt.Println(isEqual)
 
@@ -87,22 +90,45 @@ type container_network_receive_bytes_total struct {
 	Value                                                                                                            float32
 }
 
-func randomCNRBT() container_network_receive_bytes_total {
-	cnrbt := container_network_receive_bytes_total{
-		"Container",
-		"Endpoint",
-		"Id",
-		"Instance",
-		"Interface",
-		"Job",
-		"Metrics_Path",
-		"Name",
-		"Namespace",
-		"Node",
-		"Pod",
-		"Prometheus",
-		"Service",
-		123.42,
+func randomCnrbt() func() *container_network_receive_bytes_total {
+	cnrbtOptions := struct {
+		container, endpoint, id, instance, interface_, job, metricsPath, name, namespace, node, pod, prometheus, service []string
+	}{
+		container:   []string{"container1", "container2", "container3"},
+		endpoint:    []string{"endpoint1", "endpoint2", "endpoint3"},
+		id:          []string{"id1", "id2", "id3"},
+		instance:    []string{"instance1", "instance2", "instance3"},
+		interface_:  []string{"interface1", "interface2", "interface3"},
+		job:         []string{"job1", "job2", "job3"},
+		metricsPath: []string{"metricsPath1", "metricsPath2", "metricsPath3"},
+		name:        []string{"name1", "name2", "name3"},
+		namespace:   []string{"namespace1", "namespace2", "namespace3"},
+		node:        []string{"node1", "node2", "node3"},
+		pod:         []string{"pod1", "pod2", "pod3"},
+		prometheus:  []string{"prometheus1", "prometheus2", "prometheus3"},
+		service:     []string{"service1", "service2", "service3"},
 	}
-	return cnrbt
+
+	randOption := func(slice []string) string {
+		return slice[rand.Intn(len(slice))]
+	}
+
+	return func() *container_network_receive_bytes_total {
+		return &container_network_receive_bytes_total{
+			randOption(cnrbtOptions.container),
+			randOption(cnrbtOptions.endpoint),
+			randOption(cnrbtOptions.id),
+			randOption(cnrbtOptions.instance),
+			randOption(cnrbtOptions.interface_),
+			randOption(cnrbtOptions.job),
+			randOption(cnrbtOptions.metricsPath),
+			randOption(cnrbtOptions.name),
+			randOption(cnrbtOptions.namespace),
+			randOption(cnrbtOptions.node),
+			randOption(cnrbtOptions.pod),
+			randOption(cnrbtOptions.prometheus),
+			randOption(cnrbtOptions.service),
+			rand.Float32(),
+		}
+	}
 }
