83f6e41f3776d10653c56d2818fcc1aec030927a
PeterYurkovich <pyurkovi@redhat.com>
2024-03-25 11:19:25 -0400

    Swap to real example data

--- cnrbt.go
+++ cnrbt.go
@@ -15,99 +15,95 @@ func create_cnrbt_tree() *node {
 	root_node.addNextLevel([]*node{
 		{
 			label:     "container",
-			generator: func() (string, error) { return "container1", nil },
-		},
-		{
-			label:     "container",
-			generator: func() (string, error) { return "container2", nil },
+			generator: func() (string, error) { return "POD	", nil },
 		},
 	})
 
 	root_node.addNextLevel([]*node{
 		{
 			label:     "endpoint",
-			generator: func() (string, error) { return "endpoint1", nil },
+			generator: func() (string, error) { return "https-metrics	", nil },
 		},
 	})
 
 	root_node.addNextLevel([]*node{
 		{
-			label:     "id",
-			generator: func() (string, error) { return "id1", nil },
+			label: "id",
+			generator: func() (string, error) {
+				return "/kubepods.slice/kubepods-burstable.slice/kubepods-burstable-podaaaaaaaaaaaaaaaaaaaaaaa.slice/aaaa-aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa", nil
+			},
 		},
 	})
 
 	root_node.addNextLevel([]*node{
 		{
 			label:     "instance",
-			generator: func() (string, error) { return "instance1", nil },
+			generator: func() (string, error) { return "00.0.000.000:00000", nil },
 		},
 	})
 
 	root_node.addNextLevel([]*node{
 		{
 			label:     "interface",
-			generator: func() (string, error) { return "interface1", nil },
+			generator: func() (string, error) { return "eth0", nil },
 		},
 	})
 
 	root_node.addNextLevel([]*node{
 		{
 			label:     "job",
-			generator: func() (string, error) { return "job1", nil },
+			generator: func() (string, error) { return "kubelet", nil },
 		},
 	})
 
 	root_node.addNextLevel([]*node{
 		{
 			label:     "metrics_path",
-			generator: func() (string, error) { return "metrics_path1", nil },
+			generator: func() (string, error) { return "/metrics/cadvisor", nil },
 		},
 	})
 
 	root_node.addNextLevel([]*node{
 		{
-			label:     "name",
-			generator: func() (string, error) { return "name1", nil },
+			label: "name",
+			generator: func() (string, error) {
+				return "k8s_POD_alertmanager-main-0_openshift-monitoring_aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa_a", nil
+			},
 		},
 	})
 
 	root_node.addNextLevel([]*node{
 		{
 			label:     "namespace",
-			generator: func() (string, error) { return "namespace1", nil },
+			generator: func() (string, error) { return "openshift-monitoring", nil },
 		},
 	})
 
 	root_node.addNextLevel([]*node{
 		{
 			label:     "node",
-			generator: func() (string, error) { return "node1", nil },
+			generator: func() (string, error) { return "ip-00-0-000-000.ec2.internal", nil },
 		},
 	})
 
 	root_node.addNextLevel([]*node{
 		{
 			label:     "pod",
-			generator: func() (string, error) { return "pod1", nil },
+			generator: func() (string, error) { return "alertmanager-main-0", nil },
 		},
 	})
 
 	root_node.addNextLevel([]*node{
 		{
 			label:     "prometheus",
-			generator: func() (string, error) { return "prometheus1", nil },
+			generator: func() (string, error) { return "openshift-monitoring/k8s", nil },
 		},
 	})
 
 	root_node.addNextLevel([]*node{
 		{
 			label:     "service",
-			generator: func() (string, error) { return "service1", nil },
-		},
-		{
-			label:     "service",
-			generator: func() (string, error) { return "service2", nil },
+			generator: func() (string, error) { return "kubelet", nil },
 		},
 	})
 
